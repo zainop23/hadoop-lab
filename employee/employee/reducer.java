1095b84a02c73732f989a3eb3a87a6f2a079372d
zainop23 <anwerbasha@Zains-Mac.local>
2025-11-13 19:59:13 +0530

    added folders

---
+++ employee/employee/reducer.java
@@ -0,0 +1,18 @@
+package employee;
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
+public class reducer extends MapReduceBase implements Reducer<Text,DoubleWritable,Text,DoubleWritable>{
+    public void reduce(Text key, Iterator<DoubleWritable> value, OutputCollector<Text,DoubleWritable> output, Reporter r)throws IOException{
+        int count = 0;
+        Double sum = 0.0;
+        while(value.hasNext()){
+            sum += value.next().get();
+            count+=1;
+        }
+        output.collect(new Text(key + " Average"), new DoubleWritable(sum/count));
+        output.collect(new Text(key + " Count"), new DoubleWritable(count));
+    }
+}
---
+++ insurance/insurance/mapper.java
@@ -0,0 +1,12 @@
+package insurance;
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
+public class mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable>{
+    public void map(LongWritable key, Text value, OutputCollector<Text,IntWritable> output, Reporter r)throws IOException {
+        String[] line = value.toString().split(","); 
+        output.collect(new Text(line[2]), new IntWritable(1));
+    }
+}
